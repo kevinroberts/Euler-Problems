e319d03143f43270046c3d9796b22b6e933fa8f5
Kevin Roberts <kevin.roberts@icfolson.com>
2019-01-04 13:53:41 -0600

    add 4th solution

--- public/javascripts/euler/04_Largest_palindrome_product.js
+++ public/javascripts/euler/04_Largest_palindrome_product.js
@@ -1,10 +1,53 @@
-function largestPalindromeProduct(numberOfDigits) {
+function largestPalindromeProduct () {
+  let num1 = 999;
+  let num2 = 999;
+  let largestProduct = 0;
 
-  return numberOfDigits;
+  for (let i = 0; i <= 999; i++) {
+    num1--;
+    const product1 = num1 * num2;
+    if (checkPalindrome(product1)) {
+      if (largestProduct < product1) {
+        largestProduct = product1;
+      }
+    }
+    for (let j = 0; j <= 999; j++) {
+      if (j > 0 && num1 > 0) {
+        const product2 = num1 * j;
+        if (checkPalindrome(product2)) {
+          if (largestProduct < product2) {
+            largestProduct = product2;
+          }
+        }
+      }
+    }
+  }
+  return largestProduct;
+}
+
+function checkPalindrome (num) {
+  let strNum = num.toString();
+  let isPal = true;
+  if (strNum.length < 2) {
+    return true;
+  }
+
+  for (let i = 0; i <= strNum.length / 2; i++) {
+    const lead = strNum.charAt(i);
+
+    const edge = strNum.charAt((strNum.length - i) - 1);
+
+    if (lead !== edge) {
+      isPal = false;
+    }
+
+  }
+
+  return isPal;
 }
 
 // START OF NON SOLUTION CODE
-$(function() {
+$(function () {
   const id = '#04';
   const description = 'A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.\n' +
     '\n' +
@@ -12,8 +55,8 @@ $(function() {
 
   $(id).find('div').html(description.replace(/(?:\r\n|\r|\n)/g, '<br>'));
 
-  $(id).find('button').click(function() {
-    const result = largestPalindromeProduct(3);
+  $(id).find('button').click(function () {
+    const result = largestPalindromeProduct();
     $(id).find('textarea').html(result);
   });
 });
