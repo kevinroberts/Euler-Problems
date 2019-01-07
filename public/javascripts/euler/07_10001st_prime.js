6885b2e09b181f2e0610200b001f3720cc81d5fe
Kevin Roberts <kevin.roberts@icfolson.com>
2019-01-07 10:14:13 -0600

    added 7th solution

---
+++ public/javascripts/euler/07_10001st_prime.js
@@ -0,0 +1,35 @@
+function primeOfN(n) {
+  let prime = 0;
+  let primeCount = 0;
+
+  for (let i = 1; i <= 99999999; i++) {
+    // See solution #03 JS for isPrime function
+    if (isPrime(i)) {
+      primeCount++;
+
+      if (primeCount === n) {
+        prime = i;
+        break;
+      }
+    }
+  }
+  return prime;
+}
+
+// START OF NON SOLUTION CODE
+$(function () {
+  const id = '#07';
+  const description = 'By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.\n' +
+    '\n' +
+    'What is the 10 001st prime number?';
+
+  $(id).find('div').html(description.replace(/(?:\r\n|\r|\n)/g, '<br>'));
+
+  $(id).find('button').click(function () {
+    console.time('primeN');
+    const result = primeOfN(10001);
+    console.timeEnd('primeN');
+    $(id).find('textarea').html(result);
+  });
+});
+// END NON-SOLUTION CODE
