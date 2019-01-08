ae976d8f027f463edd08d8443fc7f34a1391a338
Kevin Roberts <kevin.roberts10@gmail.com>
2019-01-07 18:47:03 -0600

    added solution 10

---
+++ public/javascripts/euler/10_Summation_of_primes.js
@@ -0,0 +1,30 @@
+function sumOfPrimes() {
+  let primes = [];
+  for (let i = 1; i <= 2000000; i++) {
+    // See solution #03 JS for isPrime function
+    if (isPrime(i)) {
+      primes.push(i);
+    }
+  }
+
+  const sum = _.reduce(primes, function(memo, num){ return memo + num; }, 0);
+  return sum;
+}
+
+// START OF NON SOLUTION CODE
+$(function () {
+  const id = '#10';
+  const description = 'The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.\n' +
+    '\n' +
+    'Find the sum of all the primes below two million.';
+
+  $(id).find('div').html(description.replace(/(?:\r\n|\r|\n)/g, '<br>'));
+
+  $(id).find('button').click(function () {
+    console.time('sumOfPrimes');
+    const result = sumOfPrimes();
+    console.timeEnd('sumOfPrimes');
+    $(id).find('textarea').html(result);
+  });
+});
+// END NON-SOLUTION CODE
