6a388ccfdb500e1aae6f4f6dff70121c66af4568
Kevin Roberts <kevin.roberts10@gmail.com>
2019-01-13 19:34:53 -0600

    solution 18

--- public/javascripts/euler/18_Maximum_path_sum_I.js
+++ public/javascripts/euler/18_Maximum_path_sum_I.js
@@ -1,128 +1,23 @@
-Array.prototype.max = function() {
-  return Math.max.apply(null, this);
-};
 
-function maxPathSum1(triangle) {
-  const maxDigits = [];
-  // add the top number from the triangle
-  maxDigits.push(triangle[0][0]);
-
-  let indexOfLastNumber = 0;
-  for (let i = 1; i < triangle.length; i += 1) {
-    const currentRow = triangle[i];
-    const indexToFindNextNumber = indexOfLastNumber;
-
-    const firstChoice = triangle[i][indexToFindNextNumber];
-    let secondChoiceIndex = indexToFindNextNumber + 1;
-    let secondChoice;
-    if (secondChoiceIndex >= (currentRow.length - 1) && currentRow.length > 2) {
-      secondChoiceIndex = indexToFindNextNumber - 1;
-      secondChoice = triangle[i][secondChoiceIndex];
-    } else {
-      secondChoice = triangle[i][secondChoiceIndex];
-    }
-    if (firstChoice > secondChoice) {
-      indexOfLastNumber = indexToFindNextNumber;
-      maxDigits.push(firstChoice);
-    } else {
-      indexOfLastNumber = indexToFindNextNumber + 1;
-      maxDigits.push(secondChoice);
+function maxPathSum1(triangle, rows) {
+  const tri = triangle;
+  // loop for bottom-up calculation
+  for (let i = rows - 1; i >= 0; i -= 1) {
+    for (let j = 0; j <= i; j += 1) {
+      // for each element, check both
+      // elements just below the number
+      // and below right to the number
+      // add the maximum of them to it
+      if (tri[i + 1][j] > tri[i + 1][j + 1]) {
+        tri[i][j] += tri[i + 1][j];
+      } else {
+        tri[i][j] += tri[i + 1][j + 1];
+      }
     }
   }
-  console.log(maxDigits);
-
-  let maxSum = maxDigits.reduce((total, num) => total + num);
-  let newMax = 0;
-  let iterations = 20000;
-
-  while (iterations > 0) {
-    const newMaxDigits = [];
-    newMaxDigits.push(triangle[0][0]);
-    iterations -= 1;
-    indexOfLastNumber = 0;
-    for (let i = 1; i < triangle.length; i += 1) {
-      const currentRow = triangle[i];
-      const indexToFindNextNumber = indexOfLastNumber;
-      let firstChoiceIndex = indexToFindNextNumber + (Math.round(Math.random()) ? 1 : -1);
-      let firstChoice;
-      if (firstChoiceIndex >= (currentRow.length - 1) && currentRow.length > 2) {
-        firstChoiceIndex = indexToFindNextNumber - 1;
-        firstChoice = triangle[i][firstChoiceIndex];
-      } else {
-        firstChoice = triangle[i][firstChoiceIndex];
-      }
-      let secondChoiceIndex = firstChoiceIndex + 1;
-      let secondChoice;
-      if (secondChoiceIndex >= (currentRow.length - 1) && currentRow.length > 2) {
-        secondChoiceIndex = firstChoiceIndex - 1;
-        secondChoice = triangle[i][secondChoiceIndex];
-      } else {
-        secondChoice = triangle[i][secondChoiceIndex];
-      }
-      if (firstChoice > secondChoice) {
-        indexOfLastNumber = firstChoiceIndex;
-        newMaxDigits.push(firstChoice);
-      } else {
-        indexOfLastNumber = secondChoiceIndex;
-        newMaxDigits.push(secondChoice);
-      }
-    }
-    newMax = newMaxDigits.reduce((total, num) => total + num);
-    if (newMax > maxSum) {
-      maxSum = newMax;
-      console.log('new max: ', newMaxDigits);
-    }
-  }
-
-  iterations = 2000;
-
-  // while (iterations > 0) {
-  //   const newMaxDigits = [];
-  //   iterations -= 1;
-  //   indexOfLastNumber = 0;
-  //   const lastLength = triangle[triangle.length - 1].length - 1;
-  //   newMaxDigits.push(triangle[triangle.length - 1][Math.floor(Math.random() * lastLength)]);
-  //
-  //   for (let i = triangle.length - 2; i >= 0; i -= 1) {
-  //     const currentRow = triangle[i];
-  //     const indexToFindNextNumber = indexOfLastNumber;
-  //     let firstChoiceIndex = indexToFindNextNumber + (Math.round(Math.random()) ? 1 : 0);
-  //     let firstChoice;
-  //     if (firstChoiceIndex >= (currentRow.length - 1) && currentRow.length > 2) {
-  //       firstChoiceIndex = indexToFindNextNumber - 1;
-  //       firstChoice = triangle[i][firstChoiceIndex];
-  //     } else {
-  //       firstChoice = triangle[i][firstChoiceIndex];
-  //     }
-  //     let secondChoiceIndex = firstChoiceIndex + 1;
-  //     let secondChoice;
-  //     if (secondChoiceIndex >= (currentRow.length - 1) && currentRow.length > 2) {
-  //       secondChoiceIndex = firstChoiceIndex - 1;
-  //       secondChoice = triangle[i][secondChoiceIndex];
-  //     } else {
-  //       secondChoice = triangle[i][secondChoiceIndex];
-  //     }
-  //     if (typeof firstChoice === 'undefined') {
-  //       firstChoice = 0;
-  //     }
-  //     if (typeof secondChoice === 'undefined') {
-  //       secondChoice = 0;
-  //     }
-  //     if (firstChoice > secondChoice) {
-  //       indexOfLastNumber = firstChoiceIndex;
-  //       newMaxDigits.push(firstChoice);
-  //     } else {
-  //       indexOfLastNumber = secondChoiceIndex;
-  //       newMaxDigits.push(secondChoice);
-  //     }
-  //   }
-  //   newMax = newMaxDigits.reduce((total, num) => total + num);
-  //   if (newMax > maxSum) {
-  //     maxSum = newMax;
-  //     console.log('new max2: ', newMaxDigits);
-  //   }
-  // }
-  return maxSum;
+  // return the top element
+  // which stores the maximum sum
+  return tri[0][0];
 }
 
 // START OF NON SOLUTION CODE
@@ -142,24 +37,25 @@ window.jQuery(function ($) {
 
   $(id).find('button').click(function () {
     console.time('maxPathSum1');
+    // triangle converted to a regular matrix (empty spots replaced with zero)
     const triangle = [
-      [75],
-      [95, 64],
-      [17, 47, 82],
-      [18, 35, 87, 10],
-      [20, 4, 82, 47, 65],
-      [19, 1, 23, 75, 3, 34],
-      [88, 2, 77, 73, 7, 63, 67],
-      [99, 65, 4, 28, 6, 16, 70, 92],
-      [41, 41, 26, 56, 83, 40, 80, 70, 33],
-      [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
-      [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
-      [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
-      [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
-      [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+      [75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [95, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [17, 47, 82, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [18, 35, 87, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [20, 4, 82, 47, 65, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [19, 1, 23, 75, 3, 34, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [88, 2, 77, 73, 7, 63, 67, 0, 0, 0, 0, 0, 0, 0, 0],
+      [99, 65, 4, 28, 6, 16, 70, 92, 0, 0, 0, 0, 0, 0, 0],
+      [41, 41, 26, 56, 83, 40, 80, 70, 33, 0, 0, 0, 0, 0, 0],
+      [41, 48, 72, 33, 47, 32, 37, 16, 94, 29, 0, 0, 0, 0, 0],
+      [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14, 0, 0, 0, 0],
+      [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57, 0, 0, 0],
+      [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48, 0, 0],
+      [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31, 0],
       [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23],
     ];
-    const result = maxPathSum1(triangle);
+    const result = maxPathSum1(triangle, 14);
     console.timeEnd('maxPathSum1');
     $(id).find('textarea').html(result);
   });
