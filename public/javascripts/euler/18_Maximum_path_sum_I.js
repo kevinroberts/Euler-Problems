c7497425f06e17646cd672eeecd77b3a9bbe2df5
Kevin Roberts <kevin.roberts10@gmail.com>
2019-01-13 19:52:28 -0600

    added alt solution for 18

--- public/javascripts/euler/18_Maximum_path_sum_I.js
+++ public/javascripts/euler/18_Maximum_path_sum_I.js
@@ -20,6 +20,18 @@ function maxPathSum1(triangle, rows) {
   return tri[0][0];
 }
 
+function maxPathSumRecursive(triangle, n, row) {
+  let currentSum = triangle[row][n];
+
+  if (row < triangle.length - 1) {
+    const left = maxPathSumRecursive(triangle, n, row + 1);
+    const right = maxPathSumRecursive(triangle, n + 1, row + 1);
+    currentSum += Math.max(left, right);
+  }
+
+  return currentSum;
+}
+
 // START OF NON SOLUTION CODE
 window.jQuery(function ($) {
   const id = '#18';
@@ -38,7 +50,7 @@ window.jQuery(function ($) {
   $(id).find('button').click(function () {
     console.time('maxPathSum1');
     // triangle converted to a regular matrix (empty spots replaced with zero)
-    const triangle = [
+    const triangleAsMatrix = [
       [75, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [95, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [17, 47, 82, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -55,7 +67,25 @@ window.jQuery(function ($) {
       [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31, 0],
       [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23],
     ];
-    const result = maxPathSum1(triangle, 14);
+    const triangle = [
+      [75],
+      [95, 64],
+      [17, 47, 82],
+      [18, 35, 87, 10],
+      [20, 4, 82, 47, 65],
+      [19, 1, 23, 75, 3, 34],
+      [88, 2, 77, 73, 7, 63, 67],
+      [99, 65, 4, 28, 6, 16, 70, 92],
+      [41, 41, 26, 56, 83, 40, 80, 70, 33],
+      [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+      [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+      [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+      [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+      [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+      [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23],
+    ];
+    //const result = maxPathSum1(triangleAsMatrix, 14);
+    const result = maxPathSumRecursive(triangle, 0, 0);
     console.timeEnd('maxPathSum1');
     $(id).find('textarea').html(result);
   });
